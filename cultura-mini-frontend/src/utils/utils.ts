e3b42a6c7cae52b062a0c8ec2b3165e8548b14fa
Ashwin-3cS <flashashwin2004@gmail.com>
2025-02-13 00:45:09 +0530

    story config setted with the activeAccount (thirdweb)

--- cultura-mini-frontend/src/utils/utils.ts
+++ cultura-mini-frontend/src/utils/utils.ts
@@ -1,21 +1,24 @@
 import { StoryClient, StoryConfig } from "@story-protocol/core-sdk";
 import { http } from "viem";
-import { privateKeyToAccount, type Account } from "viem/accounts";
+// import { privateKeyToAccount, type Account } from "viem/accounts";
+import { useActiveAccount } from "thirdweb/react";
 
 // Get private key from environment variables
-const privateKey = process.env.REACT_APP_WALLET_PRIVATE_KEY;
-if (!privateKey) {
-  throw new Error("Wallet private key not found in environment variables");
-}
+// const privateKey = process.env.REACT_APP_WALLET_PRIVATE_KEY;
+// if (!privateKey) {
+//   throw new Error("Wallet private key not found in environment variables");
+// }
+
+const account = useActiveAccount();
 
 // Create account from private key
-export const account: Account = privateKeyToAccount(
-  `0x${privateKey.replace("0x", "")}` as `0x${string}`
-);
+// export const account: Account = privateKeyToAccount(
+//   `0x${privateKey.replace("0x", "")}` as `0x${string}`
+// );
 
 // Configure Story Protocol client
 const config: StoryConfig = {
-  account: account,
+  account: account?.address,
   transport: http(process.env.REACT_APP_RPC_PROVIDER_URL),
   chainId: "aeneid",
 };
